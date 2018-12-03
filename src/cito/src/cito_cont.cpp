32609b1a319f2721534211bab6ed437e0ae37962
Aykut Onol <onol.aykut@gmail.com>
2018-12-03 17:45:35 -0500

    cito_cont class

--- src/cito/src/cito_cont.cpp
+++ src/cito/src/cito_cont.cpp
@@ -8,69 +8,75 @@
 // the bodies.
 
 // ***** CLASS TYPE ************************************************************
-// Robot and environment specific
+// Robot, environment, and physics engine specific
 
 #include "cito_cont.h"
 
-// intermediate control variables **********************************************
-
-
-//setControl: sets generalized forces on joints and bodies
-void SCvx::setControl(const mjModel* m, mjData* d, const ctrlVec_t u)
+//setControl: sets generalized forces on joints and free bodies
+void CitoCont::setControl(mjData* d, const ctrlVec_t u)
 {
-    // set control
-    for( int i=0; i<ndof; i++ )
+    // set control given the control input
+    for( int i=0; i<nact; i++ )
     {
-      d->ctrl[i] = u[i] + d->qfrc_bias[u_id[i]];
+      d->ctrl[i] = u[i] + d->qfrc_bias[act_id[i]];
     }
-    // get kcon
-    double kcon = u[ndof];
-    // contact model
-    ho = this->contactModel(m, d, kcon);
-    // set external force on the torso
-    for( int i=0; i<6; i++ )
+    // get kcon from the control input
+    for( int i=0; i<npair; i++ )
     {
-      d->xfrc_applied[mj_name2id(m, mjOBJ_BODY, "torso")*6+i] = ho[i];
+      kcon[i] = u[nact+i];
+    }
+    // contact model
+    hcon[6*nfree] = this->contactModel(d, kcon);
+    // set external forces on the free bodies
+    for( int i=0; i<nfree; i++ )
+    {
+      for( int j=0; j<6; j++ )
+      {
+        d->xfrc_applied[free_id[i]*6+j] = hcon[i*6+j];
+      }
     }
 }
 
 // contactModel: returns contact wrench given kcon
-Eigen::Matrix<double, 6, 1> SCvx::contactModel(const mjModel* m, const mjData* d, double kcon)
+Eigen::Matrix<double, 6*nfree, 1> CitoCont::contactModel(const mjData* d, double kcon)
 {
-    for( int c=0; c<ncc; c++ )
+    h.setZero();
+    // loop for each contact pair
+    for( int p_i=0; p_i<npair; p_i++ )
     {
-        // contact normal
-        for( int i=0; i<4; i++ )
+        // vectors in the world frame
+        for( int i=0; i<3; i++ )
         {
-          obj_q[i] = d->qpos[i+3];
+          p_sr[i] = d->site_xpos[spair1[p_i]*3+i];  // position of the site on the robot
+          p_se[i] = d->site_xpos[spair2[p_i]*3+i];  // position of the site in the environment
+          n_cs[i] = csn[p_i*3+i];                   // contact surface normal
         }
-        mju_rotVecQuat(cc_n, cc_n0, obj_q);
+        v_re  = p_se - p_sr;                        // vector from the end effector to the environment
         // distance
-        for( int i=0; i<3; i++ )
+        phi_e = v_re.norm();                        // Euclidean distance between the end effector and the environment
+        phi_n = v_re.dot(n_cs);                     // normal distance between the end effector and the environment
+        zeta  = tanh(phi_r*phi_e);                    // semisphere based on the Euclidean distance
+        phi_c = zeta*phi_e + (1-zeta)*phi_n;        // combined distance
+        // normal force in the contact frame
+        fn = kcon[p_i]*exp(-acon[p_i]*phi_c);
+        // contact generalized in the world frame
+        lambda = fn*n_cs;
+        // loop for each free body
+        for( int f_i=0; f_i<nfree; f_i++ )
         {
-          pcc(i) = d->site_xpos[mj_name2id(m, mjOBJ_SITE, "site_cc1")*3+i];
-          pee(i) = d->site_xpos[mj_name2id(m, mjOBJ_SITE, "site_ee")*3+i];
-          cc_n_Eig(i) = cc_n[i];
+          for( int i=0; i<3; i++ )
+          {
+            p_fb[i] = d->qpos[jfree[f_i]+i];        // position of the center of mass of the free body
+          }
+          v_ef = p_cf - p_se;                       // vector from the end effector to the center of mass of free body
+          // wrench on the free body due to the contact p_i: [lambda; cross(v_ef, lambda)]
+          h[f_i*6+0] += lambda[0];
+          h[f_i*6+1] += lambda[1];
+          h[f_i*6+2] += lambda[2];
+          h[f_i*6+3] += -v_ef[2]*lambda[1] + v_ef[1]*lambda[2];
+          h[f_i*6+4] += v_ef[2]*lambda[0]  - v_ef[0]*lambda[2];
+          h[f_i*6+5] += -v_ef[1]*lambda[0] + v_ef[0]*lambda[1];
         }
-        pee2cc = pcc-pee;
-        phi_e = sqrt(pow(pee2cc[0],2)+pow(pee2cc[1],2)+pow(pee2cc[2],2));
-        phi_n = pee2cc.dot(cc_n_Eig);
-        zeta = tanh(200*phi_e);
-        phi_c = zeta*phi_e + (1-zeta)*phi_n;
-        // normal contact force
-        fn = kcon*exp(-acon*phi_c);
-        // object wrench & joint torques
-        lambda = fn*cc_n_Eig;
-        // wrench on the object
-        for( int i=0; i<3; i++ )
-        po(i) = d->qpos[i];
-        L = pee - po;
-        Lhat <<     0,   -L[2],    L[1],
-                 L[2],       0,   -L[0],
-                -L[1],    L[0],       0;
-        W << Eigen::MatrixXd::Identity(3,3),
-             Lhat;
     }
-
-    return wrench;
+    return h;
 }
