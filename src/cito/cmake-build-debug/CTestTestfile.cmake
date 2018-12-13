37fcdf2348c317cff32e29c6c7dc4d4c7f4c4039
Aykut Onol <onol.aykut@gmail.com>
2018-12-13 11:39:44 -0500

    .gitignore update

--- src/cito/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aykut/Development/cito/src/cito
-# Build directory: /home/aykut/Development/cito/src/cito/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
