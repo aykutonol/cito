5f5a8c427d66248676224fdc6cbd45d4dbf80f79
Aykut Onol <onol.aykut@gmail.com>
2019-04-11 20:35:09 -0400

    create derivative class

---
+++ include/cito_deriv.h
@@ -0,0 +1,20 @@
+/*! Numerical Differentiation */
+/**
+ *  \brief CitoDeriv class consists of functions for taking derivatives.
+ *
+ *  This class defines functions for calculating the derivatives of the
+ *  dynamics using Pinocchio.
+ *
+ *  \author Aykut Onol
+ */
+
+#ifndef CITO_DERIV_H
+#define CITO_DERIV_H
+
+
+class cito_deriv {
+
+};
+
+
+#endif //CITO_DERIV_H
---
+++ src/cito_deriv.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aykut on 4/11/19.
+//
+
+#include "cito_deriv.h"
