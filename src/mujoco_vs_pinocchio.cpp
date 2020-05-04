6c4dd40c72607a5c2760435fa7db285bd9c6b401
Aykut Onol <onol.aykut@gmail.com>
2020-05-04 13:01:29 -0400

    evaluate and compare bias terms for random velocities

--- src/mujoco_vs_pinocchio.cpp
+++ src/mujoco_vs_pinocchio.cpp
@@ -20,27 +20,23 @@ int main(int argc, char const *argv[]) {
     /// Create data
     mjData* d = mj_makeData(m);
     /// Set the joint positions to the preset configuration in the model
-        mju_copy(d->qpos, m->key_qpos, m->nq);
+    mju_copy(d->qpos, m->key_qpos, m->nq);
+    // Set random velocity
+    std::srand(std::time(0));
+    Eigen::VectorXd vRand(m->nv);
+    vRand.setZero();
+    vRand.tail(7) = Eigen::VectorXd::Random(7);
+    mju_copy(d->qvel, vRand.data(), m->nv);
+    std::cout << "qpos: ";
+    mju_printMat(d->qpos, 1, m->nq);
+    std::cout << "qvel: ";
+    mju_printMat(d->qvel, 1, m->nv);
     /// Evaluate forward dynamics
     mj_forward(m, d);
-
-    // Pinocchio
-    pinocchio::Model model;
-    pinocchio::urdf::buildModel(paths::workspaceDir+"/src/cito/model/sawyer.urdf", model);
-    pinocchio::Data data(model);
-    /// Create data
-    Eigen::VectorXd q(model.nq), v(model.nv);
-    mju_copy(q.data(), d->qpos+7, model.nq);
-    v.setZero();
-
-    // Evaluate forward dynamics and print mass matrix
-    pinocchio::computeAllTerms(model, data, q, v);
-    std::cout << "Pinocchio:\n" << data.M << "\n";
-
-    // Get mass matrix from MuJoCo
+    /// Get mass matrix
     mjtNum* denseM = mj_stackAlloc(d, m->nv*m->nv);
     mj_fullM(m, denseM, d->qM);
-    Eigen::MatrixXd fullM(m->nv, m->nv), M(7,7);
+    Eigen::MatrixXd fullM(m->nv, m->nv), M(7, 7);
     fullM.setZero();
     for( int i=0; i<m->nv; i++ ){
         for( int j=0; j<m->nv; j++ )
@@ -48,11 +44,33 @@ int main(int argc, char const *argv[]) {
             fullM(i,j) = denseM[i*m->nv + j];
     }
     M = fullM.block<7,7>(6,6);
-    std::cout << "MuJoCo:\n" << M << "\n";
+    /// Get bias term
+    Eigen::VectorXd mj_bias(7);
+    mju_copy(mj_bias.data(), d->qfrc_bias+6, 7);
 
-    // Calculate and print mass matrix differences
+    // Pinocchio
+    /// Create model & data
+    pinocchio::Model model;
+    pinocchio::urdf::buildModel(paths::workspaceDir+"/src/cito/model/sawyer.urdf", model);
+    pinocchio::Data data(model);
+    /// Set a configuration identical to MuJoCo
+    Eigen::VectorXd q(model.nq), v(model.nv);
+    mju_copy(q.data(), d->qpos+7, model.nq);
+    mju_copy(v.data(), d->qvel+6, model.nq);
+    /// Evaluate forward dynamics
+    pinocchio::computeAllTerms(model, data, q, v);
+
+    // Print mass matrices
+    std::cout << "Pinocchio:\n" << data.M << "\n";
+    std::cout << "MuJoCo:\n" << M << "\n";
     std::cout << "Discrepancy:\n" << (M-data.M).cwiseAbs() << "\n";
     std::cout << "Max discrepancy: " << ((M-data.M).cwiseAbs()).maxCoeff() << "\n";
+
+    // Compare bias terms
+    std::cout << "\nBias terms:\nPinocchio:\t" << data.nle.transpose() << "\n";
+    std::cout << "MuJoCo:\t" << mj_bias.transpose() << "\n";
+    std::cout << "Discrepancy:\n" << (mj_bias-data.nle).cwiseAbs().transpose() << "\n";
+    std::cout << "Max discrepancy: " << ((mj_bias-data.nle).cwiseAbs()).maxCoeff() << "\n";
 
     // Shut down
     mj_deleteModel(m);
