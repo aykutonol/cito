df8b81d0202728b3dbdd20cbc59f7cbda67de9f8
Aykut Onol <onol.aykut@gmail.com>
2020-09-15 12:56:26 -0400

    add test case for util functions

--- test/test_cito.cpp
+++ test/test_cito.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-
 #include "cito_scvx.h"
 
 // MuJoCo model and data
@@ -11,15 +10,35 @@ int mj_activated = 0;
 YAML::Node params;
 
 // Assert model and data are not empty
-TEST(CitoTests, check_mujoco) {
+TEST(CitoTests, test_mujoco_init) {
     ASSERT_TRUE(mj_activated>0);
     ASSERT_TRUE(m!=NULL);
     ASSERT_TRUE(d!=NULL);
 }
 
 // Assert params file loaded
-TEST(CitoTests, check_params) {
+TEST(CitoTests, test_params_load) {
     ASSERT_FALSE(params.IsNull());
+}
+
+// Assert util functions working properly
+TEST(CitoTests, test_utils) {
+    // Instantiate a CitoParams object
+    CitoParams cp(m);
+    // Create a test vector with an expected skew-symmetric representation
+    Eigen::Vector3d v(1., 2., 3.);
+    Eigen::Matrix3d Vhat_expect;
+    Vhat_expect.setZero();
+    Vhat_expect(0,1) = -3.;
+    Vhat_expect(0,2) = 2.;
+    Vhat_expect(1,2) = -1.;
+    Vhat_expect(1,0) = 3.;
+    Vhat_expect(2,0) = -2.;
+    Vhat_expect(2,1) = 1.;
+    // Apply the util function for skew-symmetric conversion
+    Eigen::Matrix3d Vhat = cp.skew(v);
+    // Compare the resulting and expected matrices
+    ASSERT_TRUE(Vhat.isApprox(Vhat_expect));
 }
 
 int main(int argc, char **argv) {
